8736e6a4793a8172ac31d8b1047f3e21e34cf8a0
SherazShahid017 <71394541+SherazShahid017@users.noreply.github.com>
2020-11-02 18:16:43 +0000

    Update live.sh

--- live.sh
+++ live.sh
@@ -4,5 +4,5 @@ ssh 34.105.149.169 << EOF
   sudo docker pull 35.242.156.82:8082/myflask-app:latest
   git clone https://github.com/SherazShahid017/duo-task.git
   cd duo-task
-  docker-compose -d up
+  sudo docker-compose up -d
 EOF
